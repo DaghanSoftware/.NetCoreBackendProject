382f63c8f10b51f2c1ac54cee85220ea1cb0660d
Semih Dağhan <daghanex@hotmail.com>
2022-08-17 23:20:44 +0300

    JWT Altyapısının Kurulması
    1.)Jwt Helper Altyapısı Kuruldu
    2.)SecurityKeyHelper altyapısı oluşturuldu
    3.)CreateJwt operasyonu yazıldı

--- NetCoreBackend/Core/Utilities/Security/Jwt/JwtHelper.cs
+++ NetCoreBackend/Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -1,6 +1,10 @@
 ﻿using Core.Entities.Concrete;
+using Core.Utilities.Security.Encyption;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +13,34 @@ namespace Core.Utilities.Security.Jwt
 {
     public class JwtHelper : ITokenHelper
     {
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+        DateTime _accessTokenExpiration;
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
+        }
+
+        
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
-            throw new NotImplementedException();
+            var secuirtykey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
+            var signinCredentials = SigningCredentialsHelper.CreateSigningCredentials(secuirtykey);
+
+        }
+
+        public JwtSecurityToken CreateJwtSecuirtyToken(TokenOptions tokenOptions,User user,SigningCredentials signingCredentials,List<OperationClaim> operationClaims)
+        {
+            var jwt = new JwtSecurityToken(
+                issuer:tokenOptions.Issuer,
+                audience:tokenOptions.Audience,
+                expires:_accessTokenExpiration,
+                notBefore:DateTime.Now,
+                claims:operationClaims,
+                signingCredentials:signingCredentials
+                );
         }
     }
 }
